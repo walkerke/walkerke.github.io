b03ee0b795cab0e31bfdf2ea828b286c0ace0dcd
Kyle Walker <kew331@gmail.com>
2013-10-31 09:56:25 -0500

    urbangis test

---
+++ urbangis/week1/week1.R
@@ -0,0 +1,11 @@
+##Initiate presentation - Week One
+
+require(slidify)
+
+setwd("C:/Users/kylewalker/Documents/Teaching/World Regional/Lecture materials/Week 1/week1")
+
+#author("week1")
+
+slidify("index.Rmd")
+
+
