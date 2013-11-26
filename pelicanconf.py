2af21edd609b1762f288a8bd875f66259314e581
Kyle Walker <kew331@gmail.com>
2013-11-26 14:35:48 -0600

    test out new commit

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Kyle Walker'
+SITENAME = u'Teaching with Interactive Data Visualization'
+SITEURL = ''
+
+TIMEZONE = 'US/Chicago'
+
+DEFAULT_LANG = u'en_US.UTF-8 pelican'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+THEME = 'C:/Users/kylewalker/Documents/GitHub/pelican-themes/simple-bootstrap'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
